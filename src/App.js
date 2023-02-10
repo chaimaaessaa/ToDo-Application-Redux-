48dd77be0e1409f838a774c0240f5e55767d13e3
chaimaaessaa <114484989+chaimaaessaa@users.noreply.github.com>
2023-02-10 10:33:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, removeTodo } from "./Redux/Reducer";
+import TodoItem from './TodoItem';
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const count = useSelector((state) => state.todo.count);
+  const todos = useSelector((state) => state.todo.todos);
+  const dispatch = useDispatch();
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+  };
+
+  const handleTodoDone = (id) => {
+    dispatch(removeTodo(id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>todo  List</h1>
+      <form className="App-form" onSubmit={handleAddTodo}>
+        <input type="text" onInput={(e) => setInput(e.target.value)} />
+        <button type="submit">Add</button>
+      </form>
+      <div className="Todos">
+        {count > 0 &&
+          todos.map((todo) => (
+            <TodoItem key={todo.id} text={todo.text} id={todo.id} onCheck={handleTodoDone}/>
+            ))}
+        {count === 0 && <p>No todos</p>}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
